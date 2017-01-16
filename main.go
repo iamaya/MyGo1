4e6f6ae76f5e7f2b7dd004a1346b2ab27936aa6b
iamaya <imaya.kumar@yahoo.com>
2017-01-15 23:01:09 -0500

    More conditions and loop

--- main.go
+++ main.go
@@ -46,6 +46,26 @@ newSlice := []int{3,4,5,6}
 fmt.Println(newSlice)
 fmt.Println(len(newSlice))
 
+println("*********** Example for make() function ***********")
+
+slice2 := make([]int, 15)
+slice2[0] = 1
+slice2[1] = 2
+
+fmt.Println(slice2)
+
+
+println("********* Example map() make() *****")
+
+string2 := make(map[int] string, 2)
+
+string2[0] = "1222"
+string2[1] = "2222"
+string2[2] = "3222"
+string2[3] = "4222"
+
+
+fmt.Println(string2)
 
 fmt.Println("********** MAP  ***********")
 
@@ -57,4 +77,35 @@ myMap[2] = "Second"
 fmt.Println(myMap)
 
 fmt.Println(myMap[100])
+
+fmt.Println("*************** Branching *************")
+
+i := 5
+if i == 0 {
+    fmt.Println("i is 0")
+} else  {
+    fmt.Println("i is not 0")
 }
+
+if j :=5; j <=6{
+    fmt.Println("j is less than 6")
+}
+
+println("********** For Loop **********")
+
+for t:=1; t<=5; t++ {
+    println(t)
+}
+
+k:=10
+for{
+    k++
+    println(k)
+
+if k>15 {
+    break
+}
+}
+
+}
+
